15e608791af4b259204e446c259fbb3022ee7520
lucaswdm <lucas@data2.com.br>
2023-07-12 17:33:59 -0300

    Update remove-plugin-from-all-sites.php

--- remove-plugin-from-all-sites.php
+++ remove-plugin-from-all-sites.php
@@ -4,6 +4,36 @@ foreach(glob('/data/*/') as $dir)
 {
 
    if(!is_dir($dir)) continue;
+
+   $DIR_RUN = "";
+
+   if(is_file($dir . 'htdocs/wp-config.php'))
+   {
+         $SHELL = "echo {$dir}; cd {$dir}htdocs/; wp-cli plugin remove gravityforms --allow-root; ";
+         system($SHELL);
+         continue;
+   }
+
+   if(is_file($dir . 'public_html/wp-config.php'))
+   {
+         $SHELL = "echo {$dir}; cd {$dir}public_html/; wp-cli plugin remove gravityforms --allow-root; ";
+         system($SHELL);
+         continue;
+   }
+
+   if(is_file($dir . 'public/wp-config.php'))
+   {
+         $SHELL = "echo {$dir}; cd {$dir}public/; wp-cli plugin remove gravityforms --allow-root; ";
+         system($SHELL);
+         continue;
+   }
+
+   if(is_file($dir . 'wp-config.php'))
+   {
+         $SHELL = "echo {$dir}; cd {$dir}; wp-cli plugin remove gravityforms --allow-root; ";
+         system($SHELL);
+         continue;
+   }
      
-   echo $dir . PHP_EOL;
+   #echo $dir . PHP_EOL;
 }
